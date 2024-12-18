c3b9e834af337bcc61602664097fa1a3a6cac2f1
AndreaPetrone <iis.petrone.andrea@gmail.com>
2024-12-18 15:32:34 +0100

    Update complex.R

--- R/complex.R
+++ R/complex.R
@@ -5,6 +5,7 @@ function(x) {
   else print("this is a negative number") 
   } 
 
+
 complex <- function(x) {
   if(x>0){
   print("this is a positive number")
@@ -12,16 +13,6 @@ complex <- function(x) {
   else print("this is a negative number") 
   } 
 
-# if number is > 0 is a positive, if < 0 negative
-# print is the function to state something
-# after the statement put the }
-
-complex(5)
-[1] "this is a positive number"
-complex(-1)
-[1] "this is a negative number"
-
-# but if i put 0 it says it is a negative number and this is wrong
 
 complex <- function(x) {
   if (x>0) {
@@ -34,10 +25,6 @@ complex <- function(x) {
     print("this is just zero! nor positive nor negative! Have you studied maths at school?")
     }
   } 
-
-# else means "tutto il resto che non ho stabilito con if" 
-# else is the last part of the function, only "else", the { } you can use them or not, better to use them 
-# with else at the end we say that every other possibility (x==0) is what is in the print
 
 
 

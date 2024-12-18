485c87823b2dd7dc16fe0ef5f0855c5f52abc2e1
AndreaPetrone <iis.petrone.andrea@gmail.com>
2024-12-18 15:35:37 +0100

    Update simplestuff.R

--- R/simplestuff.R
+++ R/simplestuff.R
@@ -1,19 +1,4 @@
-function (x,y) { 
-  z = x+y
-  return(z)
-  }
-# takes 2 arguments (x and y) and summs them up to get z
-# the function to make a function is "function"
-
 simplestuff <- function (x,y) { 
   z = x+y
   return(z)
   }
-
-simplestuff(1,2)
-[1] 3  # on R we have the result of the sum in the function
-
-a <- 2 
-b <- 3 
-simplestuff(a,b)
-[1] 5 # we can also assign numbers to letters
